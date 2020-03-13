1449b33b7ba32ff0821cbd663bb299376f03c159
MateKoncz <matthewkoncy@gmail.com>
2020-03-13 22:39:22 +0100

    Internationalization function #1: i18n and language select function

--- src/js/MainContainer.jsx
+++ src/js/MainContainer.jsx
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react'
 import NavBar from './NavBar';
 import Credits from './Credits';
 import Rules from './Rules';
 import Game from './Game';
+import i18n from "./i18n";
+
 import {
   HashRouter,
   Switch,
   Route
 } from "react-router-dom";
 
-function MainContainer() {
-  return (
-    <HashRouter>
-      <div className="MainContainer">
-        <NavBar />
-      </div>
-      <Switch>
-        <Route path="/szabalyzat">
-          <Rules />
-        </Route>
-        <Route path="/keszito">
-          <Credits />
-        </Route>
-        <Route path="/">
-          <Game />
-        </Route>
-      </Switch>
-    </HashRouter>
-  );
-}
+export default class MainContainer extends Component {
 
-export default MainContainer;
+  constructor(){
+    super();
+    this.state = {
+      currentLang: 'hu'
+    };
+  }
+
+  render() {
+    return (
+        <HashRouter>
+          <div className="MainContainer">
+            <NavBar 
+              i18n={i18n} 
+              reloadLanguage={this.reloadLanguage}
+              currentLang={this.state.currentLang}
+            />
+          </div>
+          <Switch>
+            <Route path="/szabalyzat">
+              <Rules  
+                i18n={i18n} 
+                currentLang={this.state.currentLang}
+              />
+            </Route>
+            <Route path="/keszito">
+              <Credits 
+                i18n={i18n} 
+                currentLang={this.state.currentLang}
+              />
+            </Route>
+            <Route path="/">
+              <Game 
+                i18n={i18n} 
+                currentLang={this.state.currentLang}
+              />
+            </Route>
+          </Switch>
+        </HashRouter>
+    );
+  }
+  reloadLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+    this.setState({
+      currentLang: lang
+    });
+  }
+}
