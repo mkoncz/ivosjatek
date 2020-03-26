08c8c91f364d6a7e21a9253e86fe387216766c6a
MateKoncz <matthewkoncy@gmail.com>
2020-03-26 21:16:11 +0100

    Rules #13: More rules added. Terms of use added.
    Not localized yet.

--- src/js/Rules.jsx
+++ src/js/Rules.jsx
@@ -1,13 +1,29 @@
+// Import React module.
 import React from 'react';
 
-function Rules() {
+/**
+ * List of the rules.
+ * 
+ * @param {Object} props.i18n Configured i18next object. It is used for the localization.
+ * @param {string} props.currentLang Shortened version of the language.
+ */
+function Rules(props) {
   return (
-   <div className="rules-frame">
+    <div className="rules-frame">
+      <h2>Megkötések</h2>
       <p>A játék csak 18 éven felüliek számára engedélyezett.</p>
+      <p>A játék csak saját felelősségre használható.</p>
+      <p>A játék során történő alkoholfogyasztás nem kötelező, pusztán lehetőség.</p>
+      <p>Alkoholfogyasztás esetén minden résztvevő törekedjen a mértékletes alkoholfogyasztásra!</p>
       <p>A feladatok végrehajtása nem kötelező.</p>
-      <p>Minden résztvevő törekedjen a mértékletes alkoholfogyasztásra! </p>
-   </div>
+      <h2>Játékszabályok</h2>
+      <p>Kattints/koppints a kártya hátlapjára.</p>
+      <p>Olvasd fel a kártya előlapján lévő feladatot.</p>
+      <p>A játékosok hajtsák végre a feladatot (ha szeretnék).</p>
+      <p>A "Következő" gombbal húzd elő a következő lapot.</p>
+    </div>
   );
 }
 
+// exporting component
 export default Rules;
