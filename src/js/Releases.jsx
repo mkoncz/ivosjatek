4c12bc948fbba6aa3f077cd8ca92b45ae734c818
MateKoncz <matthewkoncy@gmail.com>
2020-04-01 18:58:34 +0200

    Typo fixed in the code documentation of Releases module (#12)

--- src/js/Releases.jsx
+++ src/js/Releases.jsx
@@ -9,15 +9,15 @@ import * as en_note_resource from '../locales/release_notes_en.json'
  * Release notes of the application.
  * 
  * @param {Object} props.i18n Configured i18next object. It is used for the localization.
- * @param {string} props.currentLang Shortened version of the language.
+ * @param {string} props.currentLanguage Shortened version of the language.
  */
  const Releases = props => {
 
   // Localized list of the release notes.
   // Note entries contain a title and a list of changes.
-  let notes = props.currentLang === "hu" ? hun_note_resource.notes : en_note_resource.notes;
+  let notes = props.currentLanguage === "hu" ? hun_note_resource.notes : en_note_resource.notes;
 
-  // Iterate and print release notes. 
+  // Iterates and prints release notes. 
   // Listing starts from the newest entry.
   return (
     <div className="releases-frame">
