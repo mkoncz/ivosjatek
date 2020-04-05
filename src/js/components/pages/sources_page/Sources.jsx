fd260af94ce8b369d4ae8b015f8cbd36e938a876
MateKoncz <matthewkoncy@gmail.com>
2020-04-05 15:48:14 +0200

    List sources with bullet points on the "Sources" page. (#31)

--- src/js/components/pages/sources_page/Sources.jsx
+++ src/js/components/pages/sources_page/Sources.jsx
@@ -13,38 +13,40 @@ const Sources = props => {
   return (
     <div className="page-frame">
       <h3>{props.i18n.t("source.sources_title")}:</h3>
-      <SourceDetail
-        i18n={props.i18n}
-        localizedName={props.i18n.t("source.source_cocktail")}
-        sourceURL="https://www.onlinewebfonts.com/icon/480147"
-        sourceName="Icon Fonts"
-        licenceURL="https://creativecommons.org/licenses/by/3.0/"
-        licenceType="CC BY 3.0"
-      />
-      <SourceDetail
-        i18n={props.i18n}
-        localizedName={props.i18n.t("source.source_hu_flag")}
-        sourceURL="https://www.flickr.com/photos/80497449@N04/7378164364"
-        sourceName="Nicolas Raymond"
-        licenceURL="https://creativecommons.org/licenses/by/2.0/"
-        licenceType="CC BY 2.0"
-      />
-      <SourceDetail
-        i18n={props.i18n}
-        localizedName={props.i18n.t("source.source_en_flag")}
-        sourceURL="https://www.publicdomainpictures.net/hu/view-image.php?image=136202"
-        sourceName="Dawn Hudson"
-        licenceURL="https://creativecommons.org/publicdomain/zero/1.0/"
-        licenceType="CC BY 1.0"
-      />
-      <SourceDetail
-        i18n={props.i18n}
-        localizedName={"Animate.css "+ props.i18n.t("source.library")}
-        sourceURL="https://daneden.github.io/animate.css/"
-        sourceName="Daniel Eden"
-        licenceURL="https://github.com/daneden/animate.css/blob/master/LICENSE"
-        licenceType="MIT Licence"
-      />
+      <ul>
+        <SourceDetail
+          i18n={props.i18n}
+          localizedName={props.i18n.t("source.source_cocktail")}
+          sourceURL="https://www.onlinewebfonts.com/icon/480147"
+          sourceName="Icon Fonts"
+          licenceURL="https://creativecommons.org/licenses/by/3.0/"
+          licenceType="CC BY 3.0"
+        />
+        <SourceDetail
+          i18n={props.i18n}
+          localizedName={props.i18n.t("source.source_hu_flag")}
+          sourceURL="https://www.flickr.com/photos/80497449@N04/7378164364"
+          sourceName="Nicolas Raymond"
+          licenceURL="https://creativecommons.org/licenses/by/2.0/"
+          licenceType="CC BY 2.0"
+        />
+        <SourceDetail
+          i18n={props.i18n}
+          localizedName={props.i18n.t("source.source_en_flag")}
+          sourceURL="https://www.publicdomainpictures.net/hu/view-image.php?image=136202"
+          sourceName="Dawn Hudson"
+          licenceURL="https://creativecommons.org/publicdomain/zero/1.0/"
+          licenceType="CC BY 1.0"
+        />
+        <SourceDetail
+          i18n={props.i18n}
+          localizedName={"Animate.css " + props.i18n.t("source.library")}
+          sourceURL="https://daneden.github.io/animate.css/"
+          sourceName="Daniel Eden"
+          licenceURL="https://github.com/daneden/animate.css/blob/master/LICENSE"
+          licenceType="MIT Licence"
+        />
+      </ul>
     </div>
   );
 }
