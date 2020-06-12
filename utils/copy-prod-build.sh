2a7dbf830dd0e4566d47e4e0c04f6425f7fa883b
MateKoncz <matthewkoncy@gmail.com>
2020-06-13 00:39:11 +0200

    Create script that replaces old prod-build with the new one at the ivosjatek-page respository (#97)

---
+++ utils/copy-prod-build.sh
@@ -0,0 +1,9 @@
+rm -r ./../../ivosjatek-page/static
+rm ./../../ivosjatek-page/asset-manifest.json
+rm ./../../ivosjatek-page/index.html
+rm ./../../ivosjatek-page/manifest.json
+rm ./../../ivosjatek-page/robots.txt
+rm ./../../ivosjatek-page/precache-mani*
+rm ./../../ivosjatek-page/favicon.ico
+rm ./../../ivosjatek-page/service-worker.js
+mv ./../build/* ./../../ivosjatek-page/
