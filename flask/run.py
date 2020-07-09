e6626be465a9a7b3040aed40c52613e0b0dfa711
MateKoncz <matthewkoncy@gmail.com>
2020-07-09 18:28:45 +0200

    Set the new static and template folder for the Flask application (#118)

--- flask/run.py
+++ flask/run.py
@@ -4,8 +4,8 @@ import os.path
 from flask import Flask
 from flask import render_template
 
-template_folder = os.path.abspath('./../build')
-static_folder = os.path.abspath('./../build')
+template_folder = os.path.abspath('./../react/build')
+static_folder = os.path.abspath('./../react/build')
 app = Flask(__name__, static_folder=static_folder, template_folder=template_folder, static_url_path='/')
 
 @app.route('/')
