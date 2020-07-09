fc9ebf55dc94032ad3618b201010fa12362b9422
MateKoncz <matthewkoncy@gmail.com>
2020-07-09 18:01:47 +0200

    Add Flask back-end for server-side routing (#117)

---
+++ flask/run.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os.path
+
+from flask import Flask
+from flask import render_template
+
+template_folder = os.path.abspath('./../build')
+static_folder = os.path.abspath('./../build')
+app = Flask(__name__, static_folder=static_folder, template_folder=template_folder, static_url_path='/')
+
+@app.route('/')
+@app.route('/contact')
+@app.route('/rules')
+@app.route('/sources')
+@app.route('/sponsors')
+@app.route('/releases')
+def route():
+    return render_template('/index.html')
