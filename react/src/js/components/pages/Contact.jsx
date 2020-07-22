84e2fcc77d565343b33351b0f94955a19cf7ebb4
MateKoncz <matthewkoncy@gmail.com>
2020-07-23 00:23:26 +0200

    Add dynamic canonical link and auto language resolver on Contact page (#130)

--- react/src/js/components/pages/Contact.jsx
+++ react/src/js/components/pages/Contact.jsx
@@ -1,5 +1,5 @@
 // Import ReactJS module.
-import React from "react";
+import React, { Component } from "react";
 
 // Instagram icon iamge resource.
 import instagramImageResource from "./../../../img/insta.png";
@@ -12,23 +12,39 @@ import { Helmet } from "react-helmet";
  * The page contains the contact e-mail address of the creator.
  * 
  * @param {Object} props.i18n Configured i18next object. It is used for the localization.
+ * @param {string} props.currentLanguage Shortened version of the language. Changing the currentLanguage property triggers an update at the language change.
+ * @param {function} props.reloadLanguage Changes the language of the page.
  */
-const Contact = props => {
-  return (
-    <div className="page-frame">
-      <Helmet>
-        <title>{props.i18n.t("nav.contact")} | {props.i18n.t("nav.title")}</title>
-        <meta name="description" content={props.i18n.t("desc.contact")} />
-        <link rel="canonical" href="https://ivosjatek.hu/contact" />
-      </Helmet>
-      <h3>{props.i18n.t("contact.contact")}</h3><br></br>
-      <a href="https://www.instagram.com/ivosjatek.hu/" className="insta-ref" target="_blank" rel="noopener noreferrer">
-        <img src={instagramImageResource} className="insta-icon" alt="Instagram" />
-        IVOSJATEK.HU
-      </a>
-    </div>
-  );
-}
 
-// Export component.
-export default Contact;
+export default class Contact extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      loaded: false
+    }
+  }
+
+  componentDidMount() {
+    this.props.reloadLanguage(this.props.currentLanguage);
+  }
+
+  render() {
+    return (
+      <div className="page-frame">
+        <Helmet>
+          <title>{this.props.i18n.t("nav.contact")} | {this.props.i18n.t("nav.title")}</title>
+          <meta name="description" content={this.props.i18n.t("desc.contact")} />
+          <link rel="canonical" href={`https://ivosjatek.hu/${this.props.currentLanguage === "en" ? "en/" : ""}contact`} />
+
+        </Helmet>
+        <h3>{this.props.i18n.t("contact.contact")}</h3><br></br>
+        <a href="https://www.instagram.com/ivosjatek.hu/" className="insta-ref" target="_blank" rel="noopener noreferrer">
+          <img src={instagramImageResource} className="insta-icon" alt="Instagram" />
+          IVOSJATEK.HU
+        </a>
+      </div>
+    );
+  }
+
+}
