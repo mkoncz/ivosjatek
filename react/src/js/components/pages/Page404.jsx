b5046f17f50696b5d80aca1bf08511262ddbd4fd
MateKoncz <matthewkoncy@gmail.com>
2020-07-24 11:46:53 +0200

    Custom 404 page (#143)

---
+++ react/src/js/components/pages/Page404.jsx
@@ -0,0 +1,20 @@
+// Import ReactJS module.
+import React, { Component } from "react";
+
+/**
+ * 404 page
+ */
+export default class Page404 extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="page-frame">
+        <h1>404</h1>
+      </div>
+    );
+  }
+}
