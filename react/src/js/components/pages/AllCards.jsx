8850f8a971500fb61f459a8b5be5e02f30a0e9ba
Mate <matthewkoncy@gmail.com>
2020-09-09 08:36:16 +0200

    Create a subpage with all the cards (#139)

---
+++ react/src/js/components/pages/AllCards.jsx
@@ -0,0 +1,45 @@
+// Import ReactJS module.
+import React, { Component } from "react";
+
+
+// Import React Helmet for handling <head> attributes dinamically.
+import { Helmet } from "react-helmet";
+
+// Import question resources.
+import * as questions from "./../../../locales/questions.json"
+
+/**
+ * All cards listed.
+ * 
+ * @param {Object} props.i18n Configured i18next object. It is used for the localization.
+ * @param {string} props.currentLanguage Shortened version of the language.
+ * @param {function} props.reloadLanguage Changes the language of the page.
+ */
+export default class AllCards extends Component {
+
+  render() {
+    return (
+      <div className="page-frame">
+        <Helmet>
+          <title>{this.props.i18n.t("nav.cards")} | {this.props.i18n.t("nav.title")}</title>
+          <meta name="description" content={this.props.i18n.t("desc.cards")} />
+          <link rel="canonical" href={`https://ivosjatek.hu/${this.props.currentLanguage === "en" ? "en/" : ""}releases`} />
+          <meta name="twitter:title" content={`${this.props.i18n.t("nav.cards")}`} />
+          <meta name="twitter:description" content={this.props.i18n.t("desc.cards")} />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={`https://ivosjatek.hu/${this.props.currentLanguage === "en" ? "en/" : ""}releases`} />
+          <meta property="og:title" content={`${this.props.i18n.t("nav.cards")} | ${this.props.i18n.t("nav.title")}`} />
+          <meta property="og:description" content={this.props.i18n.t("desc.cards")} />
+          <meta property="og:locale" content={this.props.currentLanguage === "en" ? "en_US" : "hu_HU"} />
+        </Helmet>
+        {questions.default.map((question, index) => {
+          return (
+            <div key={index} className="sponsor-card white-card">
+              <h4>{question.hu.replace("%NAME%", this.props.i18n.t("game.player").toUpperCase())}</h4>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
