75ac342d76b6a6e07faa946846860075ebe7e13e
Mate <matthewkoncy@gmail.com>
2020-09-16 17:12:52 +0200

    Refactored Posts component (#202)

--- react/src/js/components/pages/posts/Posts.jsx
+++ react/src/js/components/pages/posts/Posts.jsx
@@ -1,5 +1,6 @@
 // Import ReactJS module.
 import React from "react";
+import Modal from 'react-modal';
 
 // Import React Helmet for handling <head> attributes dinamically.
 import { Helmet } from "react-helmet";
@@ -7,6 +8,14 @@ import { Helmet } from "react-helmet";
 // Import post style classes.
 import "./../../../../css/pages/posts.css";
 
+// Import adult logo.
+import adult_logo from "./../../../../img/18.png";
+
+// Import post content
+import * as posts_objects from "./../../../../locales/posts_hu.json"
+
+
+import Post from "./Post"
 
 /**
  * The page contains the list of posts.
@@ -14,90 +23,99 @@ import "./../../../../css/pages/posts.css";
  * @param {Object} props.i18n Configured i18next object. It is used for the localization.
  */
 const Posts = props => {
+
+  const [modalIsOpen, setIsOpen] = React.useState(true);
+
+  const postModalStyle = {
+      content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor: "#000",
+          textAlign: "center"
+      }
+  }
+
+  let posts = posts_objects.default.all_posts;
+
+  for (var i = posts.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = posts[i];
+    posts[i] = posts[j];
+    posts[j] = temp;
+  }
+
+  // Returns the opened post
+  const getCurrentPost = () => {
+    let post;
+    posts.forEach(element => {
+      if (element[0].type === 'meta') {
+        if (window.location.pathname.includes(element[0].slug)) {
+          post = <Post
+            postObjects={element}>
+          </Post>
+        }
+      }
+    });
+    return post;
+  }
+
+  // Returns the opened post
+  const getAllThumbs = () => {
+    return posts.map(element => {
+      return (
+        <div>
+          <a href={`/p/${element[0].slug}`}>
+            <img width="100%" src={element[0].img}
+              className="post-thumbnail" alt={props.i18n.t("nav.posts")} />
+            <div className="post-thumbnail-title-container" >
+              <h4 className="post-thumbnail-title">
+                {element[0].title}
+              </h4>
+            </div>
+          </a>
+          <div className="post-separator"></div>
+        </div>
+      )
+    });
+  }
+
   return (
     <div className="page-frame">
       <Helmet>
         <title>{props.i18n.t("nav.posts")} | {props.i18n.t("nav.title")}</title>
         <meta name="description" content={props.i18n.t("desc.posts")} />
         <link rel="canonical" href="https://ivosjatek.hu/posts" />
-        <meta name="twitter:title" content={props.i18n.t("nav.posts")} />
-        <meta name="twitter:description" content={props.i18n.t("desc.posts")} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://ivosjatek.hu/posts" />
-        <meta property="og:title" content={`${props.i18n.t("nav.posts")} | ${props.i18n.t("nav.title")}`} />
-        <meta property="og:description" content={props.i18n.t("desc.posts")} />
         <meta property="og:locale" content={props.currentLanguage === "en" ? "en_US" : "hu_HU"} />
       </Helmet>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => { setIsOpen(false) }}
+        style={postModalStyle}
+        contentLabel="18 éves"
+      >
+        <div className="post-card-big">
+          <div className="pre-card">
+            <img className="adult_logo" src={adult_logo} alt="18" />
+            <div className="question_block">
+              <h5>Kérhetnék egy személyit?</h5>
+              <p>Az ivosjatek.hu 18 éven felülieknek készült.</p>
+            </div>
+            <button
+              className="btn btn-warning btn-lg"
+              onClick={() => { setIsOpen(false) }}>
+              Felnőtt vagyok
+                        </button>
+          </div>
+        </div>
+      </Modal>
       <h1>{props.i18n.t("nav.posts")}</h1>
 
-      <a href="/p/felnottfilmes-keri-a-videoi-torleset">
-        <img src="https://instagram.fbud4-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/117313314_464886431139434_3748052878640087362_n.jpg?_nc_ht=instagram.fbud4-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=n_xYb40bbU0AX8IN_7L&oh=95da63349844fd36ac02ffd1fe490b82&oe=5F64F723"
-          className="post-thumbnail" alt={props.i18n.t("nav.posts")} />
-        <div className="post-thumbnail-title-container" >
-          <h4 className="post-thumbnail-title">
-            Majdnem kétmillióan kérik, hogy távolítsák el a netről a világ legismertebb pornósának videóit
-          </h4>
-        </div>
-      </a>
-      <div className="post-separator"></div>
-      <a className="post-a" href="/p/ketrecharcos-lesz-ukrajna-legnagyobb-mellu-noje">
-        <img src="https://scontent.fbud5-1.fna.fbcdn.net/v/t1.0-0/cp0/e15/q65/p320x320/61822127_2448520568533903_2356604974282047488_o.jpg?_nc_cat=106&_nc_sid=85a577&_nc_ohc=XR_ZFOjHTN8AX_DgrmI&_nc_ht=scontent.fbud5-1.fna&tp=3&oh=c0081426da90f2f5cd062657db2115a2&oe=5F7866C1"
-          className="post-thumbnail" alt={props.i18n.t("nav.posts")} />
-        <div className="post-thumbnail-title-container" >
-          <h4 className="post-thumbnail-title">
-            MMA harcosnak áll Ukrajna legnagyobb mellű nője
-          </h4>
-        </div>
-      </a>
-      <div className="post-separator"></div>
-      <a className="post-a" href="/p/biztositast_kotott_a_peniszere">
-        <img src="https://scontent.fbud5-1.fna.fbcdn.net/v/t1.0-9/91511753_103366491325073_6363818790119014400_n.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=aoFdlIIQmFsAX-3uhWG&_nc_ht=scontent.fbud5-1.fna&oh=a95929c8b73ee9ac61fcc98cb8bbc9e9&oe=5F77244A"
-          className="post-thumbnail" alt={props.i18n.t("nav.posts")} />
-        <div className="post-thumbnail-title-container" >
-          <h4 className="post-thumbnail-title">
-            1 millió dolláros biztosítást kötött a péniszére a férfi
-          </h4>
-        </div>
-      </a>
-      <div className="post-separator"></div>
-      <a className="post-a" href="/p/pornosnak-allt-az-autoversenyzono">
-        <img src="https://instagram.fbud5-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/103390422_170880257756271_1324934774843271870_n.jpg?_nc_ht=instagram.fbud5-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=Z0R62PPNWwAAX8t-EC_&oh=a0a337cf51e59f3fc0c5302c0ed5746e&oe=5F51DEFD"
-          className="post-thumbnail" alt={props.i18n.t("nav.posts")} />
-        <div className="post-thumbnail-title-container" >
-          <h4 className="post-thumbnail-title">
-            Pornósnak állt az autóversenyzőnő, mert abban sokkal több a pénz
-          </h4>
-        </div>
-      </a>
-      <div className="post-separator"></div>
-      <a className="post-a" href="/p/megvettek-mia-khalifa-szemuveget">
-        <img src="https://i.kym-cdn.com/entries/icons/mobile/000/024/210/mia.jpg"
-          className="post-thumbnail" alt={props.i18n.t("nav.posts")} />
-        <div className="post-thumbnail-title-container" >
-          <h4 className="post-thumbnail-title">
-            Elárverezte szemüvegét az expornós Mia Khalifa – a befolyt összeget jótékony célra ajánlotta
-          </h4>
-        </div>
-      </a>
-      <a className="post-a" href="/p/ukran-pizza-reklam">
-        <img src="https://i.kym-cdn.com/photos/images/newsfeed/001/449/412/c10.jpg"
-          className="post-thumbnail" alt={props.i18n.t("nav.posts")} />
-        <div className="post-thumbnail-title-container" >
-          <h4 className="post-thumbnail-title">
-              Ukrán pizza reklám készült a mémmé Piper Perri film mintájára. Erre ki tennétek a 18-as karikát?
-          </h4>
-        </div>
-      </a>
-      <div className="post-separator"></div>
-      <a className="post-a" href="/p/a-vilag-legdragabb-italai">
-        <img src="https://www.worldstopmost.com/wp-content/uploads/2016/08/Penfolds-Ampoule-Worlds-Most-Expensive-Alcohol-2017.jpg"
-          className="post-thumbnail" alt={props.i18n.t("nav.posts")} />
-        <div className="post-thumbnail-title-container" >
-          <h4 className="post-thumbnail-title">
-            A világ legdrágább italai - a királyok, nemesek, mágnások kiváltságai
-          </h4>
-        </div>
-      </a>
+      {getCurrentPost()}
+      {getAllThumbs()}
 
     </div>
   );
