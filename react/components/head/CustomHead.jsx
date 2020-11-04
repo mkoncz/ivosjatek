d8408d2358be10a853e79fa6db12b298fdbfdcac
Mate <matthewkoncy@gmail.com>
2020-11-04 12:11:46 +0100

    Fix default CustomHead image (#255)

--- react/components/head/CustomHead.jsx
+++ react/components/head/CustomHead.jsx
@@ -36,14 +36,14 @@ const CustomHead = props => {
       <meta name="twitter:description" content={props.desc} />
       <meta name="twitter:card" content="summary"></meta>
       <meta name="twitter:site" content="@ivosjatek" />
-      <meta name="twitter:image" content={props.imgPath == null ? "/img/yellow_logo.png" : imgPath} />
+      <meta name="twitter:image" content={props.imgPath == null ? "https://ivosjatek.hu/img/yellow_logo.png" : imgPath} />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content={props.url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={props.desc} />
       <meta property="og:locale" content={lang} />
-      <meta property="og:image" content={props.imgPath == null ? "/img/yellow_logo.png" : imgPath} />
+      <meta property="og:image" content={props.imgPath == null ? "https://ivosjatek.hu/img/yellow_logo.png" : imgPath} />
 
       {props.hasInstagram ? <script async src="//embed.redditmedia.com/widgets/platform.js" charset="UTF-8"></script> : null}
       {props.hasReddit ? <script async src="//www.instagram.com/embed.js"></script> : null}
