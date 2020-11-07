7991a50d90b327bfee40fe8585386cdc80d73a69
Mate <matthewkoncy@gmail.com>
2020-11-07 21:24:40 +0100

    Add an English drinking game (#279)

---
+++ react/pages/games/en/never-have-i-ever.tsx
@@ -0,0 +1,36 @@
+// Import React modules.
+import React, { Component } from "react";
+
+// Import components.
+import CustomHead from "../../../components/head/CustomHead";
+import DrinkingGameTemplate from "../../../components/drinking-games/DrinkingGameTemplate";
+
+// Import cookie initializer function.
+import { initLanguageCookie } from "../../../i18n";
+
+/**
+ * The page contains a selected drinking game.
+ */
+export default class SelectedDrinkingGame extends Component {
+
+  constructor(props) {
+    super(props);
+    initLanguageCookie();
+  }
+
+  render() {
+    return (
+      <div>
+        <CustomHead
+          title="Never Have I Ever"
+          desc="Never Have I Ever"
+          url="https://ivosjatek.hu/games/en/never-have-i-ever"
+          imgPath="/img/games_page/meg_soha.jpg"
+        />
+        <DrinkingGameTemplate
+          game="never-have-i-ever"
+        />
+      </div>
+    );
+  }
+}
