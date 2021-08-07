4bf5e999b88e064813ce930cd3d4e04823e9e942
Máté Koncz <matthewkoncy@gmail.com>
2021-08-08 00:02:42 +0200

    Create a pixel art menu page (#330)

--- react/pages/welcome.tsx
+++ react/pages/welcome.tsx
@@ -8,15 +8,10 @@ import CustomHead from "../components/head/CustomHead";
 // Import i18n functions.
 import { t, initLanguageCookie } from "../i18n";
 
-// Interface for state.
-interface WelcomeState {
-  activeFrame: string, // category of question
-}
-
 /**
  * Welcome page.
  */
-export default class Welcome extends Component<{}, WelcomeState> {
+export default class Welcome extends Component {
 
   constructor(props) {
     super(props);
@@ -24,12 +19,6 @@ export default class Welcome extends Component<{}, WelcomeState> {
     this.state = {
       activeFrame: "welcome"
     };
-  }
-
-  selectMode = () => {
-    this.setState({
-      activeFrame: "modes"
-    });
   }
 
   render() {
@@ -42,30 +31,58 @@ export default class Welcome extends Component<{}, WelcomeState> {
         />
         <NavBar />
         <div className="page-frame">
-          <div className="welcome">
+          <div className="menu-item-container">
             <a href="/game">
-              <div className="menu-items menu-item-1">
-                <h1 className="welcome">{t("welcome.online_game")}</h1>
+              <div className="menu-item menu-item-1">
+                <h1>{t("welcome.online_game")}</h1>
               </div>
             </a>
           </div>
-          <div className="welcome">
+          <div className="menu-item-container">
             <a href="/game-light">
-              <div className="menu-items menu-item-2">
-                <h2 className="welcome">{t("welcome.light_cards")}</h2>
+              <div className="menu-item menu-item-2">
+                <h2>{t("welcome.light_cards")}</h2>
               </div>
             </a>
           </div>
-          <div className="welcome">
+          <div className="menu-item-container">
             <a href="/games">
-              <div className="menu-items menu-item-3">
-                <h3 className="welcome">{t("welcome.other_games")}</h3>
+              <div className="menu-item menu-item-3">
+                <h3>{t("welcome.other_games")}</h3>
+              </div>
+            </a>
+          </div>
+          <div className="menu-item-container">
+            <a href="/contact">
+              <div className="menu-item menu-item-4">
+                <h4>{t("welcome.sponsors")}</h4>
+              </div>
+            </a>
+          </div>
+          <div className="menu-item-container">
+            <a href="/contact">
+              <div className="menu-item menu-item-5">
+                <h4>{t("welcome.sources")}</h4>
+              </div>
+            </a>
+          </div>
+          <div className="menu-item-container">
+            <a href="/contact">
+              <div className="menu-item menu-item-6">
+                <h4>{t("welcome.release_notes")}</h4>
+              </div>
+            </a>
+          </div>
+          <div className="menu-item-container">
+            <a href="/contact">
+              <div className="menu-item menu-item-7">
+                <h4>{t("welcome.contact")}</h4>
               </div>
             </a>
           </div>
         </div>
-        <div className="p-container">
-          <p className="welcome">{t("welcome.creator_support")} {t("welcome.help")}: &nbsp; <a href={t("welcome.help_url")}> {t("welcome.help_title")}</a> </p>
+        <div className="footer-container">
+          <p className="footer">&nbsp; {t("welcome.creator_support")} {t("welcome.help")}: <a href={t("welcome.help_url")}> {t("welcome.help_title")}</a> &nbsp; </p>
         </div>
       </div>
     );
