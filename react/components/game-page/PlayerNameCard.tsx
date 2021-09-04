0b9adbbaf12213ebaf92788420cf8a79915ed85b
Máté Koncz <matthewkoncy@gmail.com>
2021-09-04 15:57:10 +0200

    Redesign PLAYERS card (#338)

--- react/components/game-page/PlayerNameCard.tsx
+++ react/components/game-page/PlayerNameCard.tsx
@@ -1,4 +1,5 @@
 // Import React modules.
+import { State } from "@popperjs/core";
 import React, { Component } from "react";
 
 // Import translate function.
@@ -9,87 +10,112 @@ interface PlayerNameCardProps {
   replaceCard(newQuestion: boolean): any;
 }
 
+// Interface for states.
+interface PlayerNameCardStates {
+  inputValue: string;
+  players: string[];
+}
+
 /**
  * The page contains the actual Card.
  */
-export default class PlayerNameCard extends Component<PlayerNameCardProps> {
-  inputs: any;
-  //inputs: LegacyRef<HTMLInputElement>[];
-
+export default class PlayerNameCard extends Component<
+  PlayerNameCardProps,
+  PlayerNameCardStates
+> {
   constructor(props: PlayerNameCardProps) {
     super(props);
 
-    this.inputs = [];
-
-    for (let i = 0; i < 10; i++) {
-      this.inputs.push(React.createRef());
-    }
+    this.state = {
+      inputValue: "",
+      players: [],
+    };
   }
+
+  handleInputChange = (event: any) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  deletePlayer = (name: string) => {
+    this.setState({
+      players: this.state.players.filter((el) => el !== name),
+    });
+  };
 
   render() {
     return (
-      <div className="pre-card">
-        <h5 className="players-header">
+      <div className="">
+        <h5 className="c-game-card__special-content-subheader">
           <b>{t("game.player_names")}</b>
         </h5>
-        {this.generateForm()}
+        <div>
+          <label className="c-game-card__special-content-label">
+            {t("game.player")}: &nbsp;{" "}
+          </label>
+          <input
+            type="text"
+            className="c-game-card__special-content-input"
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}
+          ></input>
+        </div>
         <button
-          className="btn btn-warning btn-lg add-button"
+          className="btn c-game-card__special-content-button"
+          onClick={() => {
+            this.addPlayer();
+          }}
+        >
+          {"Add"}
+        </button>
+        {this.state.players.map((name) => {
+          return (
+            <div className="c-game-card__special-content-name" key={name}>
+              {name}
+              <span
+                className="c-game-card__special-content-delete"
+                onClick={() => this.deletePlayer(name)}
+              >
+                &nbsp;x
+              </span>
+            </div>
+          );
+        })}
+        <button
+          className="btn c-game-card__button"
           onClick={() => {
             this.addPlayersToSessionStorage();
           }}
         >
-          {t("game.done")}
+          Start
         </button>
       </div>
     );
   }
 
   /**
-   * Creates card with age check question and next button.
+   * Adds player to the players state and resets the input field.
    */
-  generateForm = () => {
-    return (
-      <form>
-        {this.inputs.map((input, index) => {
-          return (
-            <div key={index}>
-              <label key={index} className="player-name-label">
-                {t("game.player")} {index + 1}: &nbsp;{" "}
-              </label>
-              <input
-                type="text"
-                className="player-name-input"
-                ref={input}
-              ></input>
-            </div>
-          );
-        })}
-      </form>
-    );
+  addPlayer = () => {
+    if (this.state.players.length > 9) {
+      return;
+    }
+    if (this.state.players.includes(this.state.inputValue)) {
+      return;
+    }
+    this.setState({
+      players: [...this.state.players, this.state.inputValue.substring(0, 10)],
+      inputValue: "",
+    });
   };
 
   /**
    * Stores player names in SessionStorage.
    */
   addPlayersToSessionStorage = () => {
-    let that = this;
-    // Timeout needed because render() immediately replaces content and the bouncing is not done yet.
-    var players = [];
-    that.inputs.forEach((item) => {
-      if (item.current.value !== "") {
-        players.push(item.current.value);
-      }
-    });
-    if (players.length === 0) {
-      alert(t("game.please_add_player_name"));
-      return;
-    } else {
-      this.props.replaceCard(true);
-      setTimeout(() => {
-        // If the user does not give any player names, we add some default ones.
-        sessionStorage.setItem("players", JSON.stringify(players));
-      }, 450);
-    }
+    this.props.replaceCard(true);
+    setTimeout(() => {
+      // If the user does not give any player names, we add some default ones.
+      sessionStorage.setItem("players", JSON.stringify(this.state.players));
+    }, 450);
   };
 }
