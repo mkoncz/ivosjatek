3ad674a65c0c9fcec8fa77e019ebbfb85d8431e8
Máté Koncz <matthewkoncy@gmail.com>
2021-09-08 23:41:13 +0200

    Move navbar.scss to styles/components folder (#380)

--- react/pages/_app.js
+++ react/pages/_app.js
@@ -1,9 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.scss";
-import "../styles/navbar.scss";
 import "../styles/components/adult-consent.scss";
 import "../styles/components/drinking-game.scss";
 import "../styles/components/game-card.scss";
+import "../styles/components/navbar.scss";
 import "../styles/components/post.scss";
 import "../styles/pages/404.scss";
 import "../styles/pages/cards.scss";
